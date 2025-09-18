c4de844c91a010a2ec950ecd71d21b6b22a2ac57
unawarexi <unawarexi@gmail.com>
2025-09-18 16:14:33 +0100

    fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.fullstack_flutter_salesbet"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.0.13004108"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
